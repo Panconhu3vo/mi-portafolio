735e082a1352074e791bcb3f9f4cebb65cdb421f
Panconhu3vo <juan.yan3z06@gmail.com>
2025-09-22 11:46:32 -0300

    styles: Mejora card de tecnologias

--- portafolio/src/components/Technologies.jsx
+++ portafolio/src/components/Technologies.jsx
@@ -8,24 +8,46 @@ import jssvg from "../assets/icons/javascript-logo-svgrepo-com.svg";
 import pythonsvg from "../assets/icons/python-svgrepo-com.svg";
 import typescriptsvg from "../assets/icons/typescript-icon-svgrepo-com.svg";
 import sqlsvg from "../assets/icons/sql-database-generic-svgrepo-com.svg";
+
 export default function Technologies() {
     return (
         <section className="technologies">
             <Line text={"Tecnologías"} />
             <div className="tech">
-                <img className="img-tech" src={reactsvg} alt="React" />
-                <img className="img-tech" src={figmasvg} alt="Figma" />
-                <img className="img-tech" src={gitSvg} alt="Git" />
-                <img className="img-tech" src={javaSvg} alt="Java" />
-                <img className="img-tech" src={jssvg} alt="JavaScript" />
-                <img
-                    className="img-tech"
-                    src={typescriptsvg}
-                    alt="TypeScript"
-                />
-                <img className="img-tech" src={nodeSvg} alt="Node.js" />
-                <img className="img-tech" src={pythonsvg} alt="Python" />
-                <img className="img-tech" src={sqlsvg} alt="sql" />
+                <div className="tech-container">
+                    <img className="img-tech" src={reactsvg} alt="React" />
+                </div>
+                <div className="tech-container">
+                    <img className="img-tech" src={figmasvg} alt="Figma" />
+                </div>
+                <div className="tech-container">
+                    <img className="img-tech" src={gitSvg} alt="Git" />
+                </div>
+
+                <div className="tech-container">
+                    <img className="img-tech" src={javaSvg} alt="Java" />
+                </div>
+
+                <div className="tech-container">
+                    <img className="img-tech" src={jssvg} alt="JavaScript" />
+                </div>
+
+                <div className="tech-container">
+                    <img
+                        className="img-tech"
+                        src={typescriptsvg}
+                        alt="TypeScript"
+                    />
+                </div>
+                <div className="tech-container">
+                    <img className="img-tech" src={nodeSvg} alt="Node.js" />
+                </div>
+                <div className="tech-container">
+                    <img className="img-tech" src={pythonsvg} alt="Python" />
+                </div>
+                <div className="tech-container">
+                    <img className="img-tech" src={sqlsvg} alt="sql" />
+                </div>
             </div>
         </section>
     );

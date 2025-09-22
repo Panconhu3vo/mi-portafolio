cbac83e1bf5573020048113257ad0de9d8879cce
Panconhu3vo <juan.yan3z06@gmail.com>
2025-09-22 11:28:23 -0300

    fix: ancor descargar cv

--- portafolio/src/components/Hero.jsx
+++ portafolio/src/components/Hero.jsx
@@ -23,7 +23,7 @@ export default function Hero() {
             </p>
             <div className="hero-ancors">
                 <a
-                    href="./CV-Juan Yañez.docx.pdf"
+                    href="/docs/CV-Juan Yañez.docx.pdf"
                     download
                     className="btn-download">
                     Descargar CV
